f1069838c767084911afad9401927610da9c9848
Kebb <Kebbrokk@users.noreply.github.com>
2017-09-03 00:02:42 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  
+<head>
+<title>Rokket</title>
+<meta name="Rokket" content="An Idea by Kebb." />
+<script src='https://cdn1.kongregate.com/javascripts/kongregate_api.js'></script>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+  <body id="body">
+	<div id="topbar">
+		<img src="rokketbanner.png"></img>
+	</div>
+	<div id="display">
+	</div>
+  </body>
+</html>
